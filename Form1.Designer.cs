f08500c600b3c7bb6d4918ede3b2069dab4fb856
Sharon <162099766+xinnnnX@users.noreply.github.com>
2024-04-07 17:28:11 +0800

    Update to Homework 3-1
    Add 6 KeyUp functions to convert length

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,9 +48,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(142, 81);
+            this.label1.Location = new System.Drawing.Point(106, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "公分";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -58,9 +59,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(298, 81);
+            this.label2.Location = new System.Drawing.Point(224, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = "公尺";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -68,27 +70,30 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(438, 81);
+            this.label3.Location = new System.Drawing.Point(328, 65);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.Size = new System.Drawing.Size(29, 12);
             this.label3.TabIndex = 2;
             this.label3.Text = "公里";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(145, 169);
+            this.label4.Location = new System.Drawing.Point(109, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.Size = new System.Drawing.Size(29, 12);
             this.label4.TabIndex = 3;
             this.label4.Text = "英吋";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(298, 169);
+            this.label5.Location = new System.Drawing.Point(224, 135);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.Size = new System.Drawing.Size(29, 12);
             this.label5.TabIndex = 4;
             this.label5.Text = "英呎";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -96,76 +101,92 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(453, 169);
+            this.label6.Location = new System.Drawing.Point(340, 135);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(22, 15);
+            this.label6.Size = new System.Drawing.Size(17, 12);
             this.label6.TabIndex = 5;
             this.label6.Text = "碼";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(145, 262);
+            this.label7.Location = new System.Drawing.Point(109, 210);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(67, 15);
+            this.label7.Size = new System.Drawing.Size(53, 12);
             this.label7.TabIndex = 6;
             this.label7.Text = "說明文字";
             // 
             // txtCM
             // 
-            this.txtCM.Location = new System.Drawing.Point(145, 117);
+            this.txtCM.Location = new System.Drawing.Point(109, 94);
+            this.txtCM.Margin = new System.Windows.Forms.Padding(2);
             this.txtCM.Name = "txtCM";
-            this.txtCM.Size = new System.Drawing.Size(100, 25);
+            this.txtCM.Size = new System.Drawing.Size(76, 21);
             this.txtCM.TabIndex = 7;
+            this.txtCM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtCM_KeyUp);
             // 
             // txtM
             // 
-            this.txtM.Location = new System.Drawing.Point(290, 117);
+            this.txtM.Location = new System.Drawing.Point(218, 94);
+            this.txtM.Margin = new System.Windows.Forms.Padding(2);
             this.txtM.Name = "txtM";
-            this.txtM.Size = new System.Drawing.Size(100, 25);
+            this.txtM.Size = new System.Drawing.Size(76, 21);
             this.txtM.TabIndex = 8;
+            this.txtM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtM_KeyUp);
             // 
             // txtKM
             // 
-            this.txtKM.Location = new System.Drawing.Point(431, 117);
+            this.txtKM.Location = new System.Drawing.Point(323, 94);
+            this.txtKM.Margin = new System.Windows.Forms.Padding(2);
             this.txtKM.Name = "txtKM";
-            this.txtKM.Size = new System.Drawing.Size(100, 25);
+            this.txtKM.Size = new System.Drawing.Size(76, 21);
             this.txtKM.TabIndex = 9;
+            this.txtKM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtKM_KeyUp);
             // 
             // txtIn
             // 
-            this.txtIn.Location = new System.Drawing.Point(145, 212);
+            this.txtIn.Location = new System.Drawing.Point(109, 170);
+            this.txtIn.Margin = new System.Windows.Forms.Padding(2);
             this.txtIn.Name = "txtIn";
-            this.txtIn.Size = new System.Drawing.Size(100, 25);
+            this.txtIn.Size = new System.Drawing.Size(76, 21);
             this.txtIn.TabIndex = 10;
+            this.txtIn.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtIn_KeyUp);
             // 
             // txtFt
             // 
-            this.txtFt.Location = new System.Drawing.Point(290, 212);
+            this.txtFt.Location = new System.Drawing.Point(218, 170);
+            this.txtFt.Margin = new System.Windows.Forms.Padding(2);
             this.txtFt.Name = "txtFt";
-            this.txtFt.Size = new System.Drawing.Size(100, 25);
+            this.txtFt.Size = new System.Drawing.Size(76, 21);
             this.txtFt.TabIndex = 11;
+            this.txtFt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFt_KeyUp);
             // 
             // txtYard
             // 
-            this.txtYard.Location = new System.Drawing.Point(431, 212);
+            this.txtYard.Location = new System.Drawing.Point(323, 170);
+            this.txtYard.Margin = new System.Windows.Forms.Padding(2);
             this.txtYard.Name = "txtYard";
-            this.txtYard.Size = new System.Drawing.Size(100, 25);
+            this.txtYard.Size = new System.Drawing.Size(76, 21);
             this.txtYard.TabIndex = 12;
+            this.txtYard.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtYard_KeyUp);
             // 
             // txtInfo
             // 
-            this.txtInfo.Location = new System.Drawing.Point(145, 300);
+            this.txtInfo.Location = new System.Drawing.Point(109, 240);
+            this.txtInfo.Margin = new System.Windows.Forms.Padding(2);
             this.txtInfo.Name = "txtInfo";
-            this.txtInfo.Size = new System.Drawing.Size(403, 25);
+            this.txtInfo.Size = new System.Drawing.Size(303, 21);
             this.txtInfo.TabIndex = 13;
             // 
             // btnAllClear
             // 
-            this.btnAllClear.Font = new System.Drawing.Font("新細明體", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnAllClear.Location = new System.Drawing.Point(112, 22);
+            this.btnAllClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnAllClear.Location = new System.Drawing.Point(84, 18);
+            this.btnAllClear.Margin = new System.Windows.Forms.Padding(2);
             this.btnAllClear.Name = "btnAllClear";
-            this.btnAllClear.Size = new System.Drawing.Size(100, 47);
+            this.btnAllClear.Size = new System.Drawing.Size(75, 38);
             this.btnAllClear.TabIndex = 15;
             this.btnAllClear.Text = "清除";
             this.btnAllClear.UseVisualStyleBackColor = true;
@@ -173,9 +194,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
             this.Controls.Add(this.btnAllClear);
             this.Controls.Add(this.txtInfo);
             this.Controls.Add(this.txtYard);
@@ -191,6 +212,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -36,5 +36,82 @@ namespace LengthCalculator
         {
 
         }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double input;
+            input = Convert.ToDouble(txtCM.Text);
+
+            txtM.Text = string.Format("{0:0.##########}", input / 100);
+            txtKM.Text = string.Format("{0:0.##########}", input / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", input / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", input / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", input / 91.44);
+        }
+
+        private void txtM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double input;
+            input = Convert.ToDouble(txtM.Text);
+            input = input * 100;
+
+            txtCM.Text = string.Format("{0:0.##########}", input);
+            txtKM.Text = string.Format("{0:0.##########}", input / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", input / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", input / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", input / 91.44);
+        }
+
+        private void txtKM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double input;
+            input = Convert.ToDouble(txtKM.Text);
+            input = input * 100000;
+
+            txtCM.Text = string.Format("{0:0.##########}", input);
+            txtM.Text = string.Format("{0:0.##########}", input / 100);
+            txtIn.Text = string.Format("{0:0.##########}", input / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", input / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", input / 91.44);
+        }
+
+        private void txtIn_KeyUp(object sender, KeyEventArgs e)
+        {
+            double input;
+            input = Convert.ToDouble(txtIn.Text);
+            input = input * 2.54;
+
+            txtCM.Text = string.Format("{0:0.##########}", input);
+            txtM.Text = string.Format("{0:0.##########}", input / 100);
+            txtKM.Text = string.Format("{0:0.##########}", input / 100000);
+            txtFt.Text = string.Format("{0:0.##########}", input / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", input / 91.44);
+        }
+
+        private void txtFt_KeyUp(object sender, KeyEventArgs e)
+        {
+            double input;
+            input = Convert.ToDouble(txtFt.Text);
+            input = input * 30.48;
+
+            txtCM.Text = string.Format("{0:0.##########}", input);
+            txtM.Text = string.Format("{0:0.##########}", input / 100);
+            txtKM.Text = string.Format("{0:0.##########}", input / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", input / 2.54);
+            txtYard.Text = string.Format("{0:0.##########}", input / 91.44);
+        }
+
+        private void txtYard_KeyUp(object sender, KeyEventArgs e)
+        {
+            double input;
+            input = Convert.ToDouble(txtYard.Text);
+            input = input * 91.44;
+
+            txtCM.Text = string.Format("{0:0.##########}", input);
+            txtM.Text = string.Format("{0:0.##########}", input / 100);
+            txtKM.Text = string.Format("{0:0.##########}", input / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", input / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", input / 30.48);
+        }
     }
 }
